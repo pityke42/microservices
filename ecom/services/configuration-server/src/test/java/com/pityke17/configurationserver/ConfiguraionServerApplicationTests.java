86ff4728a3530b894d5ac7a0f46bf572151ac0f7
pityke42 <ballaistvanw@gmail.com>
2024-06-21 14:12:29 +0200

    infrastructure, configuration server and discovery service setup

---
+++ ecom/services/configuration-server/src/test/java/com/pityke17/configurationserver/ConfiguraionServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pityke17.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableConfigServer
+class ConfiguraionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ecom/services/discovery/src/test/java/com/pityke17/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pityke17.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
